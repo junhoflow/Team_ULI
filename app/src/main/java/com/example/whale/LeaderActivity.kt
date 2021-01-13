f6f90d08b68c119803e86708d5dde9d86449cb89
Junho Kim <tikibird@naver.com>
2021-01-13 22:51:02 +0900

    겹치는 레이아웃 추가

--- app/src/main/java/com/example/whale/LeaderActivity.kt
+++ app/src/main/java/com/example/whale/LeaderActivity.kt
@@ -1,20 +1,21 @@
 package com.example.whale
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.LinearLayout
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.github.mikephil.charting.charts.LineChart
+
 
 class LeaderActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_leader)
 
-
         val layout1 = findViewById<LinearLayout>(R.id.layout1)
         layout1.setOnClickListener{
             Toast.makeText(this, "알람 ON!", Toast.LENGTH_LONG).show()
-
         }
 
     }
