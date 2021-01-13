808daac1455f9017a9d1b619122cd2d643b5ba5f
Yi Joon Choi <choi990406@gmail.com>
2021-01-13 23:27:40 +0900

    200113 겹치는거 추가

---
+++ app/src/main/java/com/example/whale/FollowerActivity.kt
@@ -0,0 +1,11 @@
+package com.example.whale
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FollowerActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_follower)
+    }
+}
