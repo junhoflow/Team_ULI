2bd02d903c7b279591a4141bcde2ef8c79f9f287
Junho Kim <tikibird@naver.com>
2021-01-13 21:56:44 +0900

    Leader 레이아웃

--- app/src/main/java/com/example/whale/LeaderActivity.kt
+++ app/src/main/java/com/example/whale/LeaderActivity.kt
@@ -2,10 +2,20 @@ package com.example.whale
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.LinearLayout
+import android.widget.Toast
 
 class LeaderActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_leader)
+
+
+        val layout1 = findViewById<LinearLayout>(R.id.layout1)
+        layout1.setOnClickListener{
+            Toast.makeText(this, "알람 ON!", Toast.LENGTH_LONG).show()
+
+        }
+
     }
 }
